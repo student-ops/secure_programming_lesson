4f53efaaa51185b4686eadf4d87ffa94f4e8f870
student-ops <gogakuec@gmail.com>
2023-11-16 13:14:05 +0900

    add deadlock example java

---
+++ class12/DeadlockExample.java
@@ -0,0 +1,42 @@
+public class DeadlockExample {
+
+    public static void main(String[] args) {
+        final Object resource1 = new Object();
+        final Object resource2 = new Object();
+
+        // スレッド1
+        Thread t1 = new Thread(() -> {
+            synchronized (resource1) {
+                System.out.println("Thread 1: Locked resource 1");
+
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                }
+
+                synchronized (resource2) {
+                    System.out.println("Thread 1: Locked resource 2");
+                }
+            }
+        });
+
+        // スレッド2
+        Thread t2 = new Thread(() -> {
+            synchronized (resource2) {
+                System.out.println("Thread 2: Locked resource 2");
+
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                }
+
+                synchronized (resource1) {
+                    System.out.println("Thread 2: Locked resource 1");
+                }
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+}
